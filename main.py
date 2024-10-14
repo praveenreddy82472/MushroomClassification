77451995438e79c340745d866cec26dabf6cf9e2
praveenreddy82472 <tumatipraveenreddy23@gmail.com>
2024-10-14 16:34:18 -0500

    setup done

---
+++ main.py
@@ -0,0 +1,7 @@
+from mushroom.configuration.mongo_db_connection import MongoDBClient
+from mushroom.data_access.mushroom_data import MushroomData
+
+if __name__ == '__main__':
+    res= MushroomData()
+    print(res)
+    
---
+++ mushroom/constant/s3_bucket.py
@@ -0,0 +1,2 @@
+TRAINING_BUCKET_NAME = "mushroom-class-pipeline-pkr"
+PREDICTION_BUCKET_NAME = "mushroom-datasource-pkr"
