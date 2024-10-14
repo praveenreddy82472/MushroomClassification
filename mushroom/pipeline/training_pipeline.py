ab42a3185bc41afade48fe1ee04d2a4aa0438395
praveenreddy82472 <tumatipraveenreddy23@gmail.com>
2024-10-14 18:03:53 -0500

    data validation done

--- mushroom/pipeline/training_pipeline.py
+++ mushroom/pipeline/training_pipeline.py
@@ -1,11 +1,12 @@
-from mushroom.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
+from mushroom.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig
 
-from mushroom.entity.artifact_entity import DataIngestionArtifact
+from mushroom.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 
 from mushroom.exception import MushroomException
 import sys,os
 from mushroom.logger import logging
 from mushroom.components.data_ingestion import DataIngestion
+from mushroom.components.data_validation import DataValidation
 
 
 class TrainPipeline:
@@ -26,12 +27,24 @@ class TrainPipeline:
             return data_ingestion_artifact
         except  Exception as e:
             raise  MushroomException(e,sys)
+        
+    def start_data_validaton(self,data_ingestion_artifact:DataIngestionArtifact)->DataValidationArtifact:
+        try:
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,
+            data_validation_config = data_validation_config
+            )
+            data_validation_artifact = data_validation.initiate_data_validation()
+            return data_validation_artifact
+        except  Exception as e:
+            raise  MushroomException(e,sys)
     
     def run_pipeline(self):
         try:
             TrainPipeline.is_pipeline_running=True
 
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validaton(data_ingestion_artifact=data_ingestion_artifact)
         except  Exception as e:
             self.sync_artifact_dir_to_s3()
             TrainPipeline.is_pipeline_running=False
